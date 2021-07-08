2ba114ff250ba1c9c5a760c9e938a255cd3e0b9f
Alvaro Luna <alvalunasan@gmail.com>
2021-07-08 13:42:51 -0400

    Fullfile to ensure proper reading tif files

--- schemas/+imaging/+utils/selectFilesFromMeanF.m
+++ schemas/+imaging/+utils/selectFilesFromMeanF.m
@@ -13,7 +13,7 @@ fprintf('estimating bleaching')
 %% load tifs and calculate mean fluorescence per frame
 scan_directory
 tifls      = dir(fullfile(scan_directory, '*tif'));
-tifls      = cellfun(@(x)([scan_directory x]),{tifls(:).name},'uniformoutput',false);
+tifls      = cellfun(@(x)(fullfile(scan_directory,x)),{tifls(:).name},'uniformoutput',false);
 tifls
 nFiles     = numel(tifls);
 meanF      = zeros(nFiles,1);
