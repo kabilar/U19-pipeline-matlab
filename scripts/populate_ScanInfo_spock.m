5f629f75e704c1a10d0a31a88476ee909ae2ed5e
Alvaro Luna <alvalunasan@gmail.com>
2022-02-14 13:34:30 -0500

    populate ScanInfo script

---
+++ scripts/populate_ScanInfo_spock.m
@@ -0,0 +1,13 @@
+function populate_ScanInfo_spock(key)
+
+%Populate Scaninfo (from recording handler pipeline)
+startup_u19_pipeline_matlab_spock
+
+if nargin < 1
+    populate(imaging_rec.ScanInfo);
+else
+    populate(imaging_rec.ScanInfo, key);
+     
+end
+
+end
